1fc2bf3b76aafd7b32198ba40d58c356a255c832
rjain37 <72268614+rjain37@users.noreply.github.com>
2021-03-15 23:53:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -18,22 +18,22 @@ function execute(command) {
 }
 async function main(){
         try{
-        table = "<!DOCTYPE html><html><header></header><body><table>";  
-        content = "JOB ID,SUBMISSION TIME,COMPLETION TIME,REASON FOR STATE,PARTITION,NODES,USER"
-        content += "17, 2021-03-08T00:58:29,2021-03-09T00:58:29, None, gpu-long, gpu1, dsingh";
-        var rows = content.split("\n");
-        var headers = rows[0].split(",");
-        if(headers != null){
-                table+="<tr>";
-                headers.forEach(headerCol => table+= "<td>" + headerCol + "</td>");
-                table+="</tr>";
-for(var i = 1; i<rows.length; i++){
-                        var rowData = rows[i].split(",");
-                        table+="<tr>";
-                        rowData.forEach(cell => table+="<td>" + cell + "</td>");
-                        table+="</tr>";
-                } 
-                table+="</table></body></html>";
+          table = "<!DOCTYPE html><html><header>table, td, th {border: 1px solid black;}</header><body><table>";  
+          content = "JOB ID,SUBMISSION TIME,COMPLETION TIME,REASON FOR STATE,PARTITION,NODES,USER"
+          content += "17, 2021-03-08T00:58:29,2021-03-09T00:58:29, None, gpu-long, gpu1, dsingh";
+          var rows = content.split("\n");
+          var headers = rows[0].split(",");
+          if(headers != null){
+                  table+="<tr>";
+                  headers.forEach(headerCol => table+= "<td>" + headerCol + "</td>");
+                  table+="</tr>";
+                  for(var i = 1; i<rows.length; i++){
+                          var rowData = rows[i].split(",");
+                          table+="<tr>";
+                          rowData.forEach(cell => table+="<td>" + cell + "</td>");
+                          table+="</tr>";
+                  } 
+                  table+="</table></body></html>";
         }        
  //content = await execute("ls");
 }catch(error){
