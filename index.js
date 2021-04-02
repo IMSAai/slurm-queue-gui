a7fdaa6971abbaae111e3a03593f5362407e8bfb
rjain37 <72268614+rjain37@users.noreply.github.com>
2021-04-02 14:53:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -20,9 +20,7 @@ function execute(command) {
 //squeue -a -r -h -o %A,%V,%e,%r,%P,%N,%u
 async function main(){
         try{
-          //table = "<!DOCTYPE html><html><header>table, td, th {border: 1px solid black;}</header><body><table>";  
           content = execute('squeue -a -r -h -o %A,%V,%e,%r,%P,%N,%u');
-          //content += "17,2021-03-08T00:58:29,2021-03-09T00:58:29,None,gpu-long,gpu1,dsingh";
           var rows = content.split("\n");
           for(var i = 0; i<rows.length; i++){
             final[i] = rows[i].split(",");
