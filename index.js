c960510272bbe80f56ad8a1980f46bf8414ce634
rjain37 <72268614+rjain37@users.noreply.github.com>
2021-04-02 17:09:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const childProcess = require("child_process");
-childProcess.config({nodeRequire: require});
 var content;
 var final;
 function execute(command) {
@@ -17,15 +16,34 @@ function execute(command) {
     });
   });
 }
+function arrayToJSON(arr){
+  var keys = arr[0];
+  var newArr = arr.slice(1, arr.length);
+
+  var formatted = [],
+  data = newArr,
+  cols = keys,
+  l = cols.length;
+  for(var i = 0; i<data.length; i++){
+    var d = data[i],
+    o={};
+      for(var j =- 0; j<l; j++)
+          o[cols[j]]=d[j];
+
+    formatted.push(o);
+  }
+  return formatted;
+}
 //squeue -a -r -h -o %A,%V,%e,%r,%P,%N,%u
 async function main(){
         try{
           content = execute('squeue -a -r -h -o %A,%V,%e,%r,%P,%N,%u');
+          //content += "17,2021-03-08T00:58:29,2021-03-09T00:58:29,None,gpu-long,gpu1,dsingh";
           var rows = content.split("\n");
           for(var i = 0; i<rows.length; i++){
             final[i] = rows[i].split(",");
           }
- //content = await execute("ls");
+          final = arrayToJSON(final);
 }catch(error){
           console.error(error);
         }
