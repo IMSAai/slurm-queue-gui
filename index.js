6d3cd42ff1e63f9dbb98bc301a3e843838d8d8b6
rjain37 <72268614+rjain37@users.noreply.github.com>
2021-04-05 20:44:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -27,19 +27,21 @@ function renderToString(source, data) {
 //squeue -a -r -h -o %A,%V,%e,%r,%P,%N,%u
 async function main(){
         try{
+          final = {
+            lines: []
+          };
+            if(content!=null){
             execute('squeue -a -r -h -o %A,%V,%e,%r,%P,%N,%u');
             //content += "17,2021-03-08T00:58:29,2021-03-09T00:58:29,None,gpu-long,gpu1,dsingh";
             var rows = content.split("\n");
             for(var i = 0; i<rows.length; i++){
               rows[i] = rows[i].split(",");
            }
-            final = {
-                lines: []
-            };
             for(var i = 0; i<rows.length; i++){
                 final.lines[i] = {jobID:rows[i][0] + '',ST:rows[i][1] + '',CT:rows[i][2] + '', REASON:rows[i][3] + '',PARTITION:rows[i][4] + '',NODES:rows[i][5] + '', USER:rows[i][6] + ''};
             }
           //something to make it in json form
+            }
           }catch(error){
               console.error(error);
           }
@@ -47,16 +49,15 @@ async function main(){
 main();
 var http = require('http');
 http.createServer(function(req, res){
-        fs.readFile('/home.handlebars', function(err,data){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.readFile('home.handlebars', function(err,data){
           if(!err){
             var source = data.toString();
-            renderToString(source, final.lines);
+            source = renderToString(source, final.lines);
+            res.end(source);
           }else{
             console.log(err);
           }
         });
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(req.url);
-        res.end();
 }).listen(8080);
-console.log('Server started on localhost:8080; press Ctrl-C to terminate...!');
+console.log('Server started on localhost:9090; press Ctrl-C to terminate...!');
