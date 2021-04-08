c9e22dd9f8c9a985254bb69b9df42c8af1dd7bca
rjain37 <72268614+rjain37@users.noreply.github.com>
2021-04-08 17:05:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const childProcess = require("child_process");
 const fs = require('fs');
 const handlebars = require('handlebars');
+const express = require('express');
+const app = express();
 var content;
 var final;
 function execute(command) {
@@ -40,15 +42,31 @@ async function main(){
             for(var i = 0; i<rows.length; i++){
                 final.lines[i] = {jobID:rows[i][0] + '',ST:rows[i][1] + '',CT:rows[i][2] + '', REASON:rows[i][3] + '',PARTITION:rows[i][4] + '',NODES:rows[i][5] + '', USER:rows[i][6] + ''};
             }
-          //something to make it in json form
-            }
+           }
           }catch(error){
               console.error(error);
           }
 }
-main();
-var http = require('http');
-http.createServer(function(req, res){
+app.get('/', (req, res) => {
+        //main();
+        try{
+          final = {
+            lines: []
+          };
+            if(content!=null){
+            execute('squeue -a -r -h -o %A,%V,%e,%r,%P,%N,%u');
+            //content += "17,2021-03-08T00:58:29,2021-03-09T00:58:29,None,gpu-long,gpu1,dsingh";
+            var rows = content.split("\n");
+            for(var i = 0; i<rows.length; i++){
+              rows[i] = rows[i].split(",");
+           }
+            for(var i = 0; i<rows.length; i++){
+                final.lines[i] = {jobID:rows[i][0] + '',ST:rows[i][1] + '',CT:rows[i][2] + '', REASON:rows[i][3] + '',PARTITION:rows[i][4] + '',NODES:rows[i][5] + '', USER:rows[i][6] + ''};
+            }
+           }
+          }catch(error){
+              console.error(error);
+          }
         res.writeHead(200, {'Content-Type': 'text/html'});
         fs.readFile('home.handlebars', function(err,data){
           if(!err){
