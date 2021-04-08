09d1d5c99b3fed5e25dd52e7ff4956dad7319cf1
rjain37 <72268614+rjain37@users.noreply.github.com>
2021-04-08 17:29:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,13 @@
-const childProcess = require("child_process");
+const exec = require('child_process').exec;
 const fs = require('fs');
 const handlebars = require('handlebars');
 const express = require('express');
 const app = express();
-const port = 8080;
 var content;
 var final;
-function execute(command) {
-  return new Promise(function(resolve, reject) {
-    childProcess.exec(command, function(error, standardOutput, standardError) {
-      if (error) {
-        reject();
-        return;
-      }
-      if (standardError) {
-        reject(standardError);
-        return;
-      }
-      content += standardOutput;
-      console.log(standardOutput);
-    });
-  });
-}
+function execute(command, callback){
+    exec(command, function(error, stdout, stderr){ callback(stdout); });
+};
 function renderToString(source, data) {
   var template = handlebars.compile(source);
   var outputString = template(data);
@@ -43,32 +29,26 @@ async function main(){
             for(var i = 0; i<rows.length; i++){
                 final.lines[i] = {jobID:rows[i][0] + '',ST:rows[i][1] + '',CT:rows[i][2] + '', REASON:rows[i][3] + '',PARTITION:rows[i][4] + '',NODES:rows[i][5] + '', USER:rows[i][6] + ''};
             }
-           }
+          //something to make it in json form
+            }
           }catch(error){
               console.error(error);
           }
 }
 app.get('/', (req, res) => {
-        //main();
-        try{
-          final = {
+        final = {
             lines: []
-          };
-            if(content!=null){
-            execute('squeue -a -r -h -o %A,%V,%e,%r,%P,%N,%u');
-            //content += "17,2021-03-08T00:58:29,2021-03-09T00:58:29,None,gpu-long,gpu1,dsingh";
-            var rows = content.split("\n");
-            for(var i = 0; i<rows.length; i++){
-              rows[i] = rows[i].split(",");
-           }
-            for(var i = 0; i<rows.length; i++){
-                final.lines[i] = {jobID:rows[i][0] + '',ST:rows[i][1] + '',CT:rows[i][2] + '', REASON:rows[i][3] + '',PARTITION:rows[i][4] + '',NODES:rows[i][5] + '', USER:rows[i][6] + ''};
-            }
-           }else{console.log("lol");}
-          }catch(error){
-              console.error(error);
-          }
+        };
+        exec('squeue -a -r -h -o %A,%V,%e,%r,%P,%N,%u', (err, stdout, stderr) => console.log(stdout));
+        let rows = content.split("\n");
+        for(var i = 0; i<rows.length; i++){
+            rows[i] = rows[i].split(",");
+        }
+        for(var i = 0; i<rows.length; i++){
+            final.lines[i] = {jobID:rows[i][0] + '',ST:rows[i][1] + '',CT:rows[i][2] + '', REASON:rows[i][3] + '',PARTITION:rows[i][4] + '',NODES:rows[i][5] + '', USER:rows[i][6] + ''};
+        }
         res.writeHead(200, {'Content-Type': 'text/html'});
+        res.writeHead(200, {'X-Content-Type-Options:': 'text/html'});
         fs.readFile('home.handlebars', function(err,data){
           if(!err){
             var source = data.toString();
@@ -80,5 +60,5 @@ app.get('/', (req, res) => {
         });
 })
 app.listen(port, () => {
-  console.log('Server started on localhost:9090; press Ctrl-C to terminate...!');
+    console.log('Server started on localhost:9090; press Ctrl-C to terminate...!');
 })
