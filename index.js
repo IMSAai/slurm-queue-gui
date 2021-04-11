461888a6c034fa3fd064b6fac8bd2bc06a54302d
rjain37 <72268614+rjain37@users.noreply.github.com>
2021-04-11 17:37:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -13,51 +13,28 @@ function renderToString(source, data) {
   return outputString;
 }
 //squeue -a -r -h -o %A,%V,%e,%r,%P,%N,%u
-async function main(){
-        try{
-          final = {
-            lines: []
-          };
-            if(content!=null){
-            execute('squeue -a -r -h -o %A,%V,%e,%r,%P,%N,%u');
-            //content += "17,2021-03-08T00:58:29,2021-03-09T00:58:29,None,gpu-long,gpu1,dsingh";
-            var rows = content.split("\n");
-            for(var i = 0; i<rows.length; i++){
-              rows[i] = rows[i].split(",");
-           }
-            for(var i = 0; i<rows.length; i++){
+app.get('/', (req, res) => {
+        exec('squeue -a -r -h -o %A,%V,%e,%r,%P,%N,%u', (err, content, stderr) => {
+            var final = {
+                lines: []
+            };
+            let rows = content.split("\n");
+            for(let i = 0; i<rows.length-1; i++){
+                rows[i] = rows[i].split(",");
+            }
+            for(let i = 0; i<rows.length-1; i++){
                 final.lines[i] = {jobID:rows[i][0] + '',ST:rows[i][1] + '',CT:rows[i][2] + '', REASON:rows[i][3] + '',PARTITION:rows[i][4] + '',NODES:rows[i][5] + '', USER:rows[i][6] + ''};
             }
-          //something to make it in json form
-            }
-          }catch(error){
-              console.error(error);
-          }
-}
-app.get('/', (req, res) => {
-        var final = {
-            lines: []
-        };
-        exec('squeue -a -r -h -o %A,%V,%e,%r,%P,%N,%u', (err, stdout, stderr) => {
-            console.log(stdout);
-            var content = stdout;
-        });
-        let rows = content.split("\n");
-        for(var i = 0; i<rows.length-1; i++){
-            rows[i] = rows[i].split(",");
-        }
-        for(var i = 0; i<rows.length-1; i++){
-            final.lines[i] = {jobID:rows[i][0] + '',ST:rows[i][1] + '',CT:rows[i][2] + '', REASON:rows[i][3] + '',PARTITION:rows[i][4] + '',NODES:rows[i][5] + '', USER:rows[i][6] + ''};
-        }
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        fs.readFile('home.handlebars', function(err,data){
-          if(!err){
-            var source = data.toString();
-            source = renderToString(source, final.lines);
-            res.end(source);
-          }else{
-            console.log(err);
-          }
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            fs.readFile('home.handlebars', function(err,data){
+              if(!err){
+                let source = data.toString();
+                source = renderToString(source, final.lines);
+                res.end(source);
+              }else{
+                console.log(err);
+              }
+            });
         });
 })
 app.listen(port, () => {
