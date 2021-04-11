3255ab9160e8a30f6d5d79a98e092a495b9bf146
rjain37 <72268614+rjain37@users.noreply.github.com>
2021-04-11 17:38:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,30 +4,34 @@ const handlebars = require('handlebars');
 const express = require('express');
 const app = express();
 const port = 8080;
-function execute(command, callback){
-    exec(command, function(error, stdout, stderr){ callback(stdout); });
-};
+
 function renderToString(source, data) {
   var template = handlebars.compile(source);
   var outputString = template(data);
   return outputString;
 }
-//squeue -a -r -h -o %A,%V,%e,%r,%P,%N,%u
+
 app.get('/', (req, res) => {
+        //executing slurm command
         exec('squeue -a -r -h -o %A,%V,%e,%r,%P,%N,%u', (err, content, stderr) => {
+            //initializing final sending variable
             var final = {
                 lines: []
             };
+            //rows of jobs
             let rows = content.split("\n");
             for(let i = 0; i<rows.length-1; i++){
                 rows[i] = rows[i].split(",");
             }
+            //putting job info into final with each column
             for(let i = 0; i<rows.length-1; i++){
                 final.lines[i] = {jobID:rows[i][0] + '',ST:rows[i][1] + '',CT:rows[i][2] + '', REASON:rows[i][3] + '',PARTITION:rows[i][4] + '',NODES:rows[i][5] + '', USER:rows[i][6] + ''};
             }
+            //header
             res.writeHead(200, {'Content-Type': 'text/html'});
             fs.readFile('home.handlebars', function(err,data){
               if(!err){
+                //standard handlebars
                 let source = data.toString();
                 source = renderToString(source, final.lines);
                 res.end(source);
