b76501127751f36a6163eba1f326c5df0c81d942
Dev Singh <dsingh@imsa.edu>
2021-04-24 10:24:45 -0500

    add api json endpoint

--- index.js
+++ index.js
@@ -36,10 +36,30 @@ app.get('/', (req, res) => {
                 source = renderToString(source, final.lines);
                 res.end(source);
               }else{
-                console.log(err);
+                console.error(err);
               }
             });
         });
+})
+app.get('/api/', (req, res) => {
+  //executing slurm command
+  exec('squeue -a -r -h -o %A,%V,%e,%r,%P,%N,%u', (err, content, stderr) => {
+      //initializing final sending variable
+      var final = {
+          lines: []
+      };
+      //rows of jobs
+      let rows = content.split("\n");
+      for(let i = 0; i<rows.length-1; i++){
+          rows[i] = rows[i].split(",");
+      }
+      //putting job info into final with each column
+      for(let i = 0; i<rows.length-1; i++){
+          final.lines[i] = {jobID:rows[i][0] + '',ST:rows[i][1] + '',CT:rows[i][2] + '', REASON:rows[i][3] + '',PARTITION:rows[i][4] + '',NODES:rows[i][5] + '', USER:rows[i][6] + ''};
+      }
+      //header
+      res.json(final)
+  });
 })
 app.listen(port, () => {
     console.log('Server started on localhost:9090; press Ctrl-C to terminate...!');
