59bdb6ab1934a41b5f6ca9dc43ee22d87f2e7ec4
Dev Singh <dsingh@imsa.edu>
2021-04-24 12:20:36 -0500

    title case state

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const exec = require('child_process').exec;
 const express = require('express');
 const app = express();
 const port = 8080;
-
+function titleCase(str) {
+  str = str.toLowerCase().split(' ');
+  for (var i = 0; i < str.length; i++) {
+    str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
+  }
+  return str.join(' ');
+}
 app.get('/api/', (req, res) => {
   //executing slurm command
-  exec('squeue -a -r -h -o %A,%V,%e,%r,%P,%N,%u', (err, content, stderr) => {
+  exec('squeue -a -r -h -o %A,%V,%e,%r,%P,%N,%u,%T', (err, content, stderr) => {
       //initializing final sending variable
       var final = {
           lines: []
@@ -17,7 +23,7 @@ app.get('/api/', (req, res) => {
       }
       //putting job info into final with each column
       for(let i = 0; i<rows.length-1; i++){
-          final.lines[i] = {jobID:rows[i][0] + '',ST:rows[i][1] + '',CT:rows[i][2] + '', REASON:rows[i][3] + '',PARTITION:rows[i][4] + '',NODES:rows[i][5] + '', USER:rows[i][6] + ''};
+          final.lines[i] = {jobID:rows[i][0] + '',ST:rows[i][1] + '',CT:rows[i][2] + '', REASON:rows[i][3] + '',PARTITION:rows[i][4] + '',NODES:rows[i][5] + '', USER:rows[i][6] + '', STATE:titleCase(rows[i][7] + '')};
           if (final.lines[i].CT === "N/A") {
             final.lines[i].CT === ""
           }
